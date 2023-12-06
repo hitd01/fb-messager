344df87bb6c22be44957c0031c67af911e4836e5
hitd01 <tranhuan.dev@gmail.com>
2023-12-06 16:44:43 +0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,8 +18,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        {children}
         <ContactInformation />
+        {children}
       </body>
     </html>
   );
